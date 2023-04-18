d07d5fcfcf4cb0f41e3e566110f1ad04c24ce00a
Luisdelaluz <luisenrique.delaluz@outlook.com>
2023-04-17 18:30:12 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/TablaController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRUDweb.Models;
+using CRUDweb.Models.ViewModels;
+
+namespace CRUDweb.Controllers
+{
+    public class TablaController : Controller
+    {
+        // GET: Tabla
+        public ActionResult Index()
+        {
+            List<ListTablaViewModel> lst;
+            using (tiendaEntities1 db = new tiendaEntities1())
+            {
+                lst = (from d in db.tabla
+                       select new ListTablaViewModel
+                       {
+                           Id = d.id,
+                           Nombre = d.nombre,
+                           Fecha_Nacimiento = d.fecha_nacimiento,
+                           Correo = d.correo
+                       }).ToList();
+
+            }
+            return View(lst);
+        }
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Nuevo(TablaViewModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (tiendaEntities1 db = new tiendaEntities1())
+                    {  
+                        var oTabla = new tabla();
+                        oTabla.nombre = model.Nombre;
+                        oTabla.correo = model.Correo;
+                        oTabla.fecha_nacimiento = model.Fecha_Nacimiento;
+                        db.tabla.Add(oTabla);
+                        db.SaveChanges();
+                    }
+                    return Redirect("~/Tabla/");
+                }
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message); 
+            }
+        }
+        public ActionResult Editar(int Id)
+        {
+            TablaViewModel model = new TablaViewModel();
+            using (tiendaEntities1 db = new tiendaEntities1())
+            {
+
+                var oTabla = db.tabla.Find(Id);
+                model.Nombre = oTabla.nombre;
+                model.Correo = oTabla.correo;
+                model.Fecha_Nacimiento = oTabla.fecha_nacimiento;
+                model.Id = oTabla.id;
+            }
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Editar(TablaViewModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (tiendaEntities1 db = new tiendaEntities1())
+                    {
+                        var oTabla = db.tabla.Find(model.Id);
+                        oTabla.correo = model.Correo;
+                        oTabla.fecha_nacimiento = model.Fecha_Nacimiento;
+                        oTabla.nombre = model.Nombre;
+
+                        db.Entry(oTabla).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                    }
+                    return Redirect("~/Tabla/");
+                }
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        [HttpGet]
+        public ActionResult Eliminar(int Id)
+        {
+            TablaViewModel model = new TablaViewModel();
+            using (tiendaEntities1 db = new tiendaEntities1())
+            {
+                var oTabla = db.tabla.Find(Id);
+                db.tabla.Remove(oTabla);
+                db.SaveChanges();
+
+            }
+            return Redirect("~/Tabla/");
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CRUDweb.MvcApplication" Language="C#" %>
